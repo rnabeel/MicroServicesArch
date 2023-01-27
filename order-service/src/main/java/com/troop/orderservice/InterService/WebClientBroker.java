d920b296d7cb82fb6e6de1b2113756b58f913bf3
Raja Nabeel Ahmed <nabeelx64@icloud.com>
2023-01-27 11:55:23 +0500

    Constants Added in Order Service

--- order-service/src/main/java/com/troop/orderservice/InterService/WebClientBroker.java
+++ order-service/src/main/java/com/troop/orderservice/InterService/WebClientBroker.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
+import static com.troop.orderservice.cfg.OrderConstants.*;
 import static com.troop.orderservice.cfg.OrderURI.WEBCLIENT_BASE_URL;
 
 @Component
@@ -17,10 +18,10 @@ public class WebClientBroker {
     public Map postRequest(String URL, String json, String ... bearertoken){
         System.out.println(bearertoken);
     MultiValueMap<String, String> header = new LinkedMultiValueMap<>();
-        header.add("Content-Type", "application/json");
-        header.add("Accept", "application/json");
-        if(bearertoken.equals("[]")) {
-        header.add("Authorization",bearertoken[0]);
+        header.add(CONTENTTYPE, APPLICATIONJSON);
+        header.add(ACCEPT, APPLICATIONJSON);
+        if(bearertoken.length!=0) {
+        header.add(AUTHORIZATION,bearertoken[0]);
         }
 
     WebClient webClient = WebClient.create(WEBCLIENT_BASE_URL);
--- order-service/src/main/java/com/troop/orderservice/cfg/OrderConstants.java
+++ order-service/src/main/java/com/troop/orderservice/cfg/OrderConstants.java
@@ -1,7 +1,12 @@
 package com.troop.orderservice.cfg;
 
 public interface OrderConstants {
-     String NOT_AVAILABLE = "is not available right now";
-     String AVAILABLE = "order is placed";
-     Boolean TRUE = true;
+    String NOT_AVAILABLE = "is not available right now";
+    String AVAILABLE = "order is placed";
+    Boolean TRUE = true;
+    String APPLICATIONJSON = "application/json";
+    String CONTENTTYPE = "Content-Type";
+    String ACCEPT = "Accept";
+    String AUTHORIZATION = "Authorization";
+
 }
